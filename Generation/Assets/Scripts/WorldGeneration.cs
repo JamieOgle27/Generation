af60710c7f0aa94bef78fd26df81d82900f4230a
JamieLeeOgle <googleman91@hotmail.com>
2024-02-16 01:35:25 +0000

    basic generation script created.
    Set up the unity project and wrote a simple script which generates planes on start (also with space but atm they just spawn on top of the existing tiles.)

---
+++ Generation/Assets/Scripts/WorldGeneration.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGeneration : MonoBehaviour
+{
+    public GameObject groundPlane;
+    public int layersToGenerate;
+    private int width;
+    private int height;
+    private int[,] gridArray;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Generate(Vector3.zero);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Generate(Vector3.zero); //We want this to be the location of the player so tiles can be generated as they move
+            //For now this is on space, eventually it will just be in update so we don't crash anything
+        }
+    }
+
+    //This is the main function that will be used to do the generation - called on Start atm, but later we can put this on a button so players can fill out settings, etc first. 
+    void Generate(Vector3 origin)
+    {
+        //ToDo: Stop tiles from spawning where they already exist
+
+        for (int i = 0; i < layersToGenerate; i++)
+        {
+            width = i; height = i;
+            gridArray = new int[i, i];
+            Debug.Log("Layer: " + i);
+
+            for (int x = -width; x <= gridArray.GetLength(0); x++)
+            {
+                for (int y = -height; y <= gridArray.GetLength(1); y++)
+                {
+                    if (Mathf.Abs(x) == width || Mathf.Abs(y) == height)
+                    {
+                        Instantiate(groundPlane, new Vector3(x, 0, y) + origin, groundPlane.transform.rotation);
+                        //Debug.Log(x + " , " + y);
+                    }
+                }
+            }
+        }
+
+    }
+
+}
